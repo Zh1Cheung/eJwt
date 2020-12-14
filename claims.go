598b31ea308f22941bae105fb958c8d315f0d736
Zh1Cheung <945503088@qq.com>
2020-12-14 22:51:42 +0800

    update claims func

--- claims.go
+++ claims.go
@@ -1,30 +1,19 @@
-package eJwt
+package ejwt
 
 import (
-	"crypto/rand"
-	"encoding/hex"
 	"encoding/json"
+	"time"
 )
 
-const (
-	TokenID   = "jti"
-	Issuer    = "iss"
-	Audience  = "aud"
-	Subject   = "sub"
-	IssuedAt  = "iat"
-	ExpiresAt = "exp"
-	NotBefore = "nbf"
-)
-
-// body information
+// Claims is the main container for our body information
 type Claims map[string]interface{}
 
-// init
+// New will initiate a new claims
 func New() *Claims {
 	return &Claims{}
 }
 
-// unmarshal to claims
+// ToClaims takes in an interface and unmarshals it to claims
 func ToClaims(struc interface{}) (Claims, error) {
 	strucBytes, _ := json.Marshal(struc)
 	var claims Claims
@@ -36,7 +25,7 @@ func ToClaims(struc interface{}) (Claims, error) {
 	return claims, nil
 }
 
-// claims to struct
+// ToStruct takes your claims and sets value to struct
 func (c Claims) ToStruct(struc interface{}) error {
 	claimsBytes, _ := json.Marshal(c)
 	err := json.Unmarshal(claimsBytes, struc)
@@ -47,132 +36,25 @@ func (c Claims) ToStruct(struc interface{}) error {
 	return nil
 }
 
-func (c Claims) AddClaim(name string, value interface{}) { c[name] = value }
+// Validate checks expiration and not before times
+func (c Claims) Validate() error {
+	now := time.Now().Unix()
 
-func (c Claims) HasClaim(name string) bool { _, ok := c[name]; return ok }
-
-func (c Claims) GetClaim(name string) interface{} { return c[name] }
-
-func (c Claims) GetClaimStr(name string) string {
-	if val, ok := c[name]; ok {
-		return val.(string)
+	// Check if not before at is set and if current time hasnt started yet
+	if c.Has(NotBeforeAt) {
+		nbf := c.GetNotBeforeAt()
+		if now < nbf {
+			return ErrTokenNotYetValid
+		}
 	}
-	return ""
-}
 
-func (c Claims) GetClaimInt(name string) int {
-	if val, ok := c[name]; ok {
-		return val.(int)
+	// Check if expiration at is set and if current time is passed
+	if c.Has(ExpiresAt) {
+		exp := c.GetExpiresAt()
+		if now >= exp {
+			return ErrTokenHasExpired
+		}
 	}
-	return 0
-}
 
-func (c Claims) GetClaimFloat(name string) float32 {
-	if val, ok := c[name]; ok {
-		return val.(float32)
-	}
-	return 0
-}
-
-func (c Claims) SetTokenID() {
-	c[TokenID] = UUID()
-}
-
-func (c Claims) GetTokenID() string {
-	if val, ok := c[TokenID]; ok {
-		return val.(string)
-	}
-	return ""
-}
-
-func (c Claims) SetIssuer(issuer string) {
-	c[Issuer] = issuer
-}
-
-func (c Claims) GetIssuer() string {
-	if val, ok := c[Issuer]; ok {
-		return val.(string)
-	}
-	return ""
-}
-
-func (c Claims) SetAudience(audience string) {
-	c[Audience] = audience
-}
-
-func (c Claims) GetAudience() string {
-	if val, ok := c[Audience]; ok {
-		return val.(string)
-	}
-	return ""
-}
-
-func (c Claims) SetSubject(subject string) {
-	c[Subject] = subject
-}
-
-func (c Claims) GetSubject() string {
-	if val, ok := c[Subject]; ok {
-		return val.(string)
-	}
-	return ""
-}
-
-func (c Claims) SetIssuedAt(issuedAt int) {
-	c[IssuedAt] = issuedAt
-}
-
-func (c Claims) GetIssuedAt() int {
-	if val, ok := c[IssuedAt]; ok {
-		return val.(int)
-	}
-	return 0
-}
-
-func (c Claims) SetExpiresAt(expiresAt int) {
-	c[ExpiresAt] = expiresAt
-}
-
-func (c Claims) GetExpiresAt() int {
-	if val, ok := c[ExpiresAt]; ok {
-		return val.(int)
-	}
-	return 0
-}
-
-func (c Claims) SetNotBeforeAt(notbeforeAt int) {
-	c[NotBefore] = notbeforeAt
-}
-
-func (c Claims) GetNotBeforeAt() int {
-	if val, ok := c[NotBefore]; ok {
-		return val.(int)
-	}
-	return 0
-}
-
-func UUID() string {
-	version := byte(4)
-	uuid := make([]byte, 16)
-	rand.Read(uuid)
-
-	// Set version
-	uuid[6] = (uuid[6] & 0x0f) | (version << 4)
-
-	// Set variant
-	uuid[8] = (uuid[8] & 0xbf) | 0x80
-
-	buf := make([]byte, 36)
-	var dash byte = '-'
-	hex.Encode(buf[0:8], uuid[0:4])
-	buf[8] = dash
-	hex.Encode(buf[9:13], uuid[4:6])
-	buf[13] = dash
-	hex.Encode(buf[14:18], uuid[6:8])
-	buf[18] = dash
-	hex.Encode(buf[19:23], uuid[8:10])
-	buf[23] = dash
-	hex.Encode(buf[24:], uuid[10:])
-
-	return string(buf)
+	return nil
 }
